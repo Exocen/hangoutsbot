99884038b40e26b47d3bd6b1ca4045203c9744af
Exocen <Exocen@users.noreply.github.com>
2018-05-30 20:42:34 +0200

    install update

--- install.sh
+++ install.sh
@@ -4,76 +4,72 @@ GREEN="32"
 WOS=""
 
 function makeItColorful {
-    echo -e "\e[$2m$1\e[0m"
+	echo -e "\e[$2m$1\e[0m"
 }
 
 function is_working {
-    if [ $? -eq 0 ];then
-        makeItColorful "Réussite : $1" $GREEN
-    else
-        makeItColorful "Echec : $1" $RED
-    fi
+	if [ $? -eq 0 ];then
+		makeItColorful "Réussite : $1" $GREEN
+	else
+		makeItColorful "Echec : $1" $RED
+	fi
 }
 
 function detectOS {
-    if [ -f /etc/lsb-release ]; then
-        OS=$(cat /etc/lsb-release | grep DISTRIB_ID | sed 's/^.*=//')
-        VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | sed 's/^.*=//')
-        if [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian" ] ;then
-            WOS="$OS"
-        fi
-    elif [ -f /etc/redhat-release ]; then
-        WOS="Fedora"
-        sudo dnf install -y --nogpgcheck https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-        sudo dnf install -y --nogpgcheck https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-    elif [ -f /etc/centos-release ]; then
-        WOS="CentOS"
-    elif [ -f /etc/debian_version ]; then
-        WOS="Debian"
-    elif [ -f /etc/arch-release ]; then
-        WOS="Arch"
-    else
-        WOS="WTF ?"
-    fi
+	if [ -f /etc/lsb-release ]; then
+		OS=$(cat /etc/lsb-release | grep DISTRIB_ID | sed 's/^.*=//')
+		VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | sed 's/^.*=//')
+		if [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian" ] ;then
+			WOS="$OS"
+		fi
+	elif [ -f /etc/redhat-release ]; then
+		WOS="Fedora"
+		sudo dnf install -y --nogpgcheck https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+		sudo dnf install -y --nogpgcheck https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	elif [ -f /etc/centos-release ]; then
+		WOS="CentOS"
+	elif [ -f /etc/debian_version ]; then
+		WOS="Debian"
+	elif [ -f /etc/arch-release ]; then
+		WOS="Arch"
+	else
+		WOS="WTF ?"
+	fi
 }
 
 # Faire un detectOS avant
 function ins {
-    all="$@" # pour fonction is_working
-    echo "Installation: $all ...."
-    if [ "$WOS" = "Ubuntu" ] || [ "$WOS" = "Debian" ] ;then
-        sudo apt update -y > /dev/null 2>&1
-        sudo apt install $@ -y # > /dev/null 2>&1
-        is_working "Installation de $all"
-    elif [ "$WOS" = "Fedora" ] ;then
-        sudo dnf update -y #> /dev/null 2>&1
-        sudo dnf install $@ -y #> /dev/null 2>&1
-        is_working "Installation de $all"
-    elif [ "$WOS" = "Arch" ] ;then
-        aurman -Syu --noedit --noconfirm $@ #> /dev/null 2>&1
-        is_working "Installation de $all"
-    else
-        makeItColorful "OS Inconnu" $RED
-    fi
+	all="$@" # pour fonction is_working
+	echo "Installation: $all ...."
+	if [ "$WOS" = "Ubuntu" ] || [ "$WOS" = "Debian" ] ;then
+		sudo apt update -y > /dev/null 2>&1
+		sudo apt install $@ -y # > /dev/null 2>&1
+		is_working "Installation de $all"
+	elif [ "$WOS" = "Fedora" ] ;then
+		sudo dnf update -y #> /dev/null 2>&1
+		sudo dnf install $@ -y #> /dev/null 2>&1
+		is_working "Installation de $all"
+	elif [ "$WOS" = "Arch" ] ;then
+		aurman -Syu --noedit --noconfirm $@ #> /dev/null 2>&1
+		is_working "Installation de $all"
+	else
+		makeItColorful "OS Inconnu" $RED
+	fi
 }
 
 
 function make {
-    if [ "$(id -u)" = "0" ]; then
-        detectOS
-        ins python python-pip # arch
-        pip3 install -r requirements.txt
-        ln -sfn $PWD /usr/local/.
-        mkdir -p ~/.local/share/hangupsbot/.
-        ln -sfn $PWD/config.json ~/.local/share/hangupsbot/.
-        cp -f examples/hangupsbot.service /etc/systemd/system/
-        systemctl enable hangupsbot.service
+	detectOS
+	ins python python-pip # arch
+	pip3 install --user -r requirements.txt
+	ln -sfn $PWD /usr/local/.
+	mkdir -p ~/.local/share/hangupsbot/.
+	ln -sfn $PWD/config.json ~/.local/share/hangupsbot/.
+	cp -f examples/hangupsbot.service /etc/systemd/system/
+	systemctl enable hangupsbot.service
 	sudo pip install ndg_httpsclient
-        # need to start one time
-        python3 hangupsbot/hangupsbot.py
-    else
-        makeItColorful 'This script must be run as root' $RED
-    fi
+	# need to start one time
+	python3 hangupsbot/hangupsbot.py
 }
 make $1
 exit 0
